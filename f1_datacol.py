27e4ce3c54e188988bedb90c1dfc11bddd5baf6d
Grant Smith <57376089+ggsmith842@users.noreply.github.com>
2020-07-09 10:23:29 -0700

    Python web scrapper

--- f1_datacol.py
+++ f1_datacol.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import pandas as pd
@@ -11,7 +11,7 @@ import seaborn as sns
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[2]:
+# In[3]:
 
 
 from urllib.request import urlopen
@@ -122,8 +122,74 @@ F1_Constructor_Table.to_csv("F1_constructor_table.csv",index=False)
 # Collect data on the best drivers of the last 70 years
 # 
 
-# In[ ]:
+# In[4]:
 
 
+url_drivers="https://en.wikipedia.org/wiki/List_of_Formula_One_Grand_Prix_winners"
+html2 =urlopen(url_drivers)
 
+
+# In[6]:
+
+
+soup2 = BeautifulSoup(html2, 'lxml')
+print(soup2.title)
+
+
+# In[7]:
+
+
+#find the table we are working with
+drivers_table = soup2.find("table",{"class":"sortable plainrowheaders wikitable"})
+
+
+# In[35]:
+
+
+Driver=[]
+Driver_Country=[]
+#First_Win=[]
+for row in drivers_table.findAll('tr'):
+        cells=row.findAll('a')
+        if len(cells)>1:
+            Driver.append(cells[1].find(text=True))
+            Driver_Country.append(cells[0].find(text=True))
+
+            
+
+
+# In[26]:
+
+
+#rank and wins are a different tag type
+Rank_driver=[]
+Wins_driver=[]
+
+for row in drivers_table.findAll('tr'):
+        cells=row.findAll('td')
+        if len(cells)>1:
+            Rank_driver.append(cells[0].find(text=True))
+            Wins_driver.append(cells[2].find(text=True))
+
+import re
+Wins_driver = re.findall("\d+",str(Wins_driver))
+Rank_driver = re.findall("\d+",str(Rank_driver))
+
+
+# In[29]:
+
+
+F1_drivers_table = pd.DataFrame()
+F1_drivers_table["Rank"] = Rank_driver
+F1_drivers_table["Driver"] = Driver
+F1_drivers_table["Driver Country"] = Driver_Country
+F1_drivers_table["Wins"] = Wins_driver
+
+print(F1_drivers_table)
+
+
+# In[36]:
+
+
+F1_drivers_table.to_csv("F1_driver_table.csv",index=False)
 
